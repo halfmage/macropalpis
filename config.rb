337ebc4898a5fb2e5427f6ad85df405a7863a7d1
Gerrit Halfmann <halfmann@freshcells.de>
2015-10-07 18:28:20 +0200

    compiled css / update config.rb / update macropalpis.scss

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 preferred_syntax = :sass
 http_path = '/'
 css_dir = 'css'
-sass_dir = 'scss'
-# images_dir = 'img'
-# javascripts_dir = 'js'
+sass_dir = 'sass'
+images_dir = 'img'
+javascripts_dir = 'js'
 relative_assets = true
 line_comments = true
 output_style = :compressed
