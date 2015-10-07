920f1f294dc3fc54cdb4eb5a7baa00b7b0131511
Gerrit Halfmann <halfmann@freshcells.de>
2015-10-07 17:21:09 +0200

    added config.rb / updated index.html

---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+# images_dir = 'img'
+# javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+output_style = :compressed
+# Sass::Script::Number.precision = 10
